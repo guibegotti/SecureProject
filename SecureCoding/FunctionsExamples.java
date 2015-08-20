5256ed7c44d87adc4b8c649812f4812fa142bc8e
Guilherme Begotti <domingos.guilherme@gmail.com>
2015-08-20 17:16:55 -0300

    Scnd

--- SecureCoding/FunctionsExamples.java
+++ SecureCoding/FunctionsExamples.java
@@ -3,13 +3,26 @@
   *a segurança dos dados do usuário e integridade da aplicação produzida!
   */
   
-public class FunctionsExamples {
+public class FunctionsExamples 
+{
 
 	/**
-	 * Creates a new instance of SymmetricEncryption, but allows the user to manually set the algorithm and mode
-	 * @author Rafael Will M. de Araujo
-	 * @param algorithm Algorithm to be used
-	 * @param mode Block cipher mode of operation (CBC is highly recommended)
+	 * The path is decoded by Uri.decode() before use, 
+	 * And canonicalized by File.getCanonicalPath() 
+	 * and checked that it is included in IMAGE_DIRECTORY
+	 * @author Guilherme Begotti
 	 */
 	 
+	private static String IMAGE_DIRECTORY = localFile.getAbsolutePath();
+  	public ParcelFileDescriptor openFile(Uri paramUri, String paramString)
+    	throws FileNotFoundException 
+		{
+    		String decodedUriString = Uri.decode(paramUri.toString());
+    		File file = new File(IMAGE_DIRECTORY, Uri.parse(decodedUriString).getLastPathSegment());
+    		if (file.getCanonicalPath().indexOf(localFile.getCanonicalPath()) != 0) 
+			{
+      			throw new IllegalArgumentException();
+    		}
+    		return ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_ONLY);
+  		}
 }
