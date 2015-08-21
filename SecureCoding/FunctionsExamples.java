9d03a190f108763c4c3a3f64b6e7b12a6e43ee87
Guilherme Begotti <domingos.guilherme@gmail.com>
2015-08-20 22:10:51 -0300

    Update FunctionsExamples.java

--- SecureCoding/FunctionsExamples.java
+++ SecureCoding/FunctionsExamples.java
@@ -135,6 +135,131 @@ public class FunctionsExamples
 	    	}
   	}
 	
+	/**
+	 * The user has to grant permission to use geolocation
+	 */
 	
+	public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions$Callback callback) {
+	        if(getBoolean("SECURITY_ENABLE_GEOLOCATION_INFORMATION", true)) {
+	            WebView.geo(this.geo).permissionShowPrompt(origin, callback);
+	        }
+	        else {
+	            callback.invoke(origin, false, false);
+	        }
+	}
 	
+	/**
+	 * Provide a secure communication channel 
+	 */
+	
+	public void generateSocket throws IOException {
+		SSLSocket sslSocket = null;
+		try 
+		{
+			SSLSocketFactory sslSocketFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+			sslSocket = (SSLSocket) sslSocketFactory.createSocket("localhost", 9999);
+			PrintWriter out = new PrintWriter(sslSocket.getOutputStream(), true);
+			BufferedReader in = new BufferedReader(new InputStreamReader(sslSocket.getInputStream()));
+			BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+			String userInput;
+			while ((userInput = stdIn.readLine()) != null) 
+			{
+				out.println(userInput);
+				System.out.println(in.readLine());
+			}
+		} 
+		finally 
+		{
+			if (sslSocket != null) 
+			{
+				try 
+				{
+					sslSocket.close();
+				} catch (IOException x) {
+					//Handle Error
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Provide a secure random number 
+	 * Ensuring a good seed;
+	 */
+	
+	public static byte[] secureRandom(int size) 
+	{
+		SecureRandom sr = new SecureRandom();
+		byte[] output = new byte[size];
+		sr.nextBytes(output);
+		return output;
+	}
+	
+	/**
+	 * Take control of the operations on your aplication
+	 * Like in database, we have to control the operations,
+	 * Watch over starvation and deadlocks.
+	 */
+	
+	public void widgetOp() 
+	{
+		synchronized (widgetList) 
+		{
+			for (Widget w : widgetList) 
+			{
+				doSomething(w);
+			}
+		}
+	}
+	
+	/**
+	 * Limit the scope of the @SuppressWarnings annotation 
+	 * to the nearest code that generates a warning.
+	 */
+	
+	@SuppressWarnings("unchecked")
+	Set s = new HashSet();
+	public final void doLogic(int a,char c) 
+	{
+		s.add(a); // Produces unchecked warning
+		s.add(c); // Produces unchecked warning
+	}
+	
+	/**
+	 * Use good algorithms and good practices to store user's passwords
+	 */
+	
+	private void setPassword(byte[] pass) throws Exception 
+	{
+		byte[] salt = generateSalt(12);
+		byte[] input = appendArrays(pass, salt);
+		MessageDigest msgDigest = MessageDigest.getInstance("SHA-512");
+		// Encode the string and salt
+		byte[] hashVal = msgDigest.digest(input);  
+		clearArray(pass);  
+		clearArray(input);
+		saveBytes(salt, "salt.bin");  
+		// Save the hash value to password.bin
+		saveBytes(hashVal,"password.bin");
+		clearArray(salt);
+		clearArray(hashVal);
+	}
+	 
+	boolean checkPassword(byte[] pass) throws Exception 
+	{
+		byte[] salt = loadBytes("salt.bin");
+		byte[] input = appendArrays(pass, salt);
+		MessageDigest msgDigest = MessageDigest.getInstance("SHA-512");
+		// Encode the string and salt
+		byte[] hashVal1 = msgDigest.digest(input);
+		clearArray(pass);
+		clearArray(input);
+		// Load the hash value stored in password.bin
+		byte[] hashVal2 = loadBytes("password.bin");
+		boolean arraysEqual = Arrays.equals(hashVal1, hashVal2);
+		clearArray(hashVal1);
+		clearArray(hashVal2);
+		return arraysEqual;
+	}
+	 
 }
